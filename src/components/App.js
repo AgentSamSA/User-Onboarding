aabacbc4bd22fd305b858adde9ce91f770053609
Samuel Lee <samuelxlee@gmail.com>
2020-11-11 13:36:08 -0800

    added state, side effects, and helper components to render a user list in the app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as yup from "yup";
 import axios from "axios";
 import './App.css';
 import Form from "./Form";
+import schema from "../validation/formSchema"; 
+
+const initialFormValues = {
+  name: "",
+  email: "",
+  password: "",
+  role: "",
+  termsOfService: false,
+}
+
+const intialFormErrors = {
+  name: "",
+  email: "",
+  password: "",
+}
+
+const initialDisabled = true;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(intialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const getUsers = () => {
+    axios
+    .get("https://reqres.in/api/users")
+    .then(res => {
+      setUsers(res.data);
+    })
+    .catch(err => {
+      console.log("something went wrong!", err);
+    });
+  }
+
+  const postNewUser = newUser => {
+    axios
+    .post("https://reqres.in/api/users", newUser)
+    .then(res => {
+      console.log(res);
+      setUsers(res, ...users);
+      setFormValues(initialFormValues);
+    })
+    .catch(err => {
+      console.log("something went wrong!", err);
+      setFormValues(initialFormValues);
+    })
+  }
+
+  const changeInput = (name, value) => {
+    yup.reach(schema, name)
+    .validate(value)
+    .then(() => {
+      setFormErrors({
+        ...formErrors,
+        [name]: "",
+      });
+    })
+    .catch((err) => {
+      setFormErrors({
+        ...formErrors,
+        [name]: err.errors[0],
+      });
+    });
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  }
+
+  const submitForm = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      role: formValues.role.trim(),
+      termsOfService: formValues.termsOfService.trim(),
+    }
+    postNewUser(newUser);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
+
   return (
+    <div>
     <Form />
+
+    {
+      users.map(user => {
+        return (
+          <pre>{user.stringify()}</pre>
+        )
+      })
+    }
+    </div>
   );
 }
 
---
+++ src/validation/formSchema.js
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+
+export default yup.object().shape({
+    name: yup
+        .string()
+        .min(2, "Must be at least 2 characters long")
+        .max(20, "Can be no more than 20 characters")
+        .required("Enter a name"),
+    email: yup
+        .string()
+        .email("Must be a valid email address")
+        .required("Enter an email address"),
+    password: yup
+        .string()
+        .min(8, "Must be at least 8 characters long")
+        .required("Enter a password"),
+    role: yup
+        .string()
+        .oneOf([], "Please select a role"),
+    termsOfService: yup.boolean().required(),
+})
