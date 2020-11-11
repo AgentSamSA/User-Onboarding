2e37a931982561297ec1312d7fe8950221682db7
Samuel Lee <samuelxlee@gmail.com>
2020-11-11 12:40:47 -0800

    set up new react app and added Form component file and schema file

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as yup from "yup";
+import axios from "axios";
+import './App.css';
+import Form from "./Form";
+
+function App() {
+  return (
+    <Form />
+  );
+}
+
+export default App;
